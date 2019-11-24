51a9792b2de732f3a1a607c54c1998818e19813c
dylan <dylanbarrios201830409@cunoc.edu.gt>
2019-11-24 12:18:07 -0600

    Pantalla principal

---
+++ src/aceitera/main/main.java
@@ -0,0 +1,17 @@
+package aceitera.main;
+import aceitera.uii.Main;
+/**
+ *
+ * @author dylan
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.setVisible(true);
+    }
+    
+}
---
+++ src/aceitera/uii/Main.java
@@ -0,0 +1,38 @@
+package aceitera.uii;
+
+/**
+ *
+ * @author dylan
+ */
+public class Main extends javax.swing.JFrame {
+
+    public Main() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton1.setText("Vender");
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(990, 80, 130, 60));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/aceitera/images/fondoFiltro.jpg"))); // NOI18N
+        jLabel1.setText("jLabel1");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1280, -1));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    // End of variables declaration//GEN-END:variables
+}
