ae87bd30fd6114c58b8fbf2bb50dddda184bfc74
Dylan Barrios <dylan@debian.domain.name>
2020-09-01 12:43:34 -0600

    Se agrega el jar con las modificaciones

--- src/aceitera/main/main.java
+++ src/aceitera/main/main.java
@@ -6,9 +6,7 @@ import aceitera.uii.administrador.Administrador;
 public class main {
 
     public static void main(String[] args) {
-//        Login login = new Login();
-//        login.setVisible(true);     
-        Administrador a = new Administrador();
-        a.setVisible(true);
+        Login login = new Login();
+        login.setVisible(true);     
     }
 }
